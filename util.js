de838a829dbb90ae1717f3e498551f3dd34edfe8
Fabian Tschirschnitz <fabian.tschirschnitz@sap.com>
2019-01-08 15:29:36 +0100

    add popups and polylines

--- util.js
+++ util.js
@@ -1,53 +1,3 @@
-// polyline decode from https://github.com/mapbox/polyline/
-var decode = function (str, precision) {
-    var index = 0,
-        lat = 0,
-        lng = 0,
-        coordinates = [],
-        shift = 0,
-        result = 0,
-        byte = null,
-        latitude_change,
-        longitude_change,
-        factor = Math.pow(10, precision || 5);
-
-    // Coordinates have variable length when encoded, so just keep
-    // track of whether we've hit the end of the string. In each
-    // loop iteration, a single coordinate is decoded.
-    while (index < str.length) {
-
-        // Reset shift, result, and byte
-        byte = null;
-        shift = 0;
-        result = 0;
-
-        do {
-            byte = str.charCodeAt(index++) - 63;
-            result |= (byte & 0x1f) << shift;
-            shift += 5;
-        } while (byte >= 0x20);
-
-        latitude_change = ((result & 1) ? ~(result >> 1) : (result >> 1));
-
-        shift = result = 0;
-
-        do {
-            byte = str.charCodeAt(index++) - 63;
-            result |= (byte & 0x1f) << shift;
-            shift += 5;
-        } while (byte >= 0x20);
-
-        longitude_change = ((result & 1) ? ~(result >> 1) : (result >> 1));
-
-        lat += latitude_change;
-        lng += longitude_change;
-
-        coordinates.push([lat / factor, lng / factor]);
-    }
-
-    return coordinates;
-};
-
 // https://www.geodatasource.com/developers/javascript
 function distance(lat1, lon1, lat2, lon2) {
     if ((lat1 == lat2) && (lon1 == lon2)) {
@@ -72,7 +22,6 @@ function distance(lat1, lon1, lat2, lon2) {
 
 if (window.util === undefined) {
     window.util = {};
-    window.util.decode = decode;
     window.util.distance = distance;
 }
 
